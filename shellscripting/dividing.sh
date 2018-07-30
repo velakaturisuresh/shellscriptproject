7d7a50dad2fe6edf04c03947f6cac1fb22c1a791
velakaturisuresh <velakaturisuresh@gmail.com>
2018-07-30 11:08:52 -0700

    adding modified file

--- shellscripting/dividing.sh
+++ shellscripting/dividing.sh
@@ -1,19 +1,32 @@
-#!/bin/sh
-
+#!/bin/sh 
+div=0
 #echo "enter a value"
 a=$1
 
 #echo "enter b value"
 b=$2
- 
-	if [ $a -gt 0 ] && [ $b -gt 0 ] && [ $a -gt $b ]
-	then 
-	div=`expr "scale=2; $a / $b" | bc`
-	echo "$div"
-	elif [ $a -gt 0 ] && [ $b -gt 0 ] && [ $b -gt $a ]
-	then
-	divi=`expr "scale=2; $b / $a" | bc`
-	echo "$divi"
-	else
-	echo "please enter correct numbers"
-	fi
+i=1
+while [ $i -eq 1 ]
+do
+if [ $a -gt 0 ] && [ $b -gt 0 ]
+then
+i=0
+        if [ $a -gt $b ]
+        then
+        div=`expr "scale=2; $a / $b" | bc`
+
+        else
+        div=`expr "scale=2; $b / $a" | bc`
+
+        fi
+else
+i=1
+echo "you given wrong values. Please enter values greater than 0"
+echo "enter a & b values"
+read num1 num2
+a=$num1
+b=$num2
+fi
+done
+echo "$div"
+
